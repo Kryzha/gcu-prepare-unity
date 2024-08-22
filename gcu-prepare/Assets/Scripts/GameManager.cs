32ab076ddfdef033b14cb8c84a5b740b9d06384f
Illia Kryzhanovskyi <ilyuhamalyuha@gmail.com>
2024-08-22 14:37:54 +0100

    Winning Levels(E09)
    Added an animation "Level complete" after player finishes the level. Also added a code to switch to the next level after level is completed.

--- gcu-prepare/Assets/Scripts/GameManager.cs
+++ gcu-prepare/Assets/Scripts/GameManager.cs
@@ -6,6 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public static bool gameHasEnded = false;
     public float delayTime = 2f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
 
     public void EndGame()
     {
